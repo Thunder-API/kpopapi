49bd1fde40e6ea4b3174b60a34353c34b4297894
Seungho <poream3387@gmail.com>
2022-03-17 09:34:38 +0900

    Make Song scraper(selenium) headless chrome

--- index.js
+++ index.js
@@ -1,55 +1,104 @@
 const express = require("express");
+const spawn = require("child_process").spawn;
 const app = express();
 const port = 8000;
+const bodyParser = require("body-parser");
 
-// let {PythonShell} = require('python-shell')
-// let songs_options = {
-//     mode: 'text',
-//     pythonPath: './scrape/env-kpop/bin/python3',
-//     pythonOptions: ['-u'],
-//     scriptPath: './scrape/kpop_scrape_project/kpop_scrape_project',
-// }
-// PythonShell.run('selenium_songs.py', options, (err, results) => {
-//     if (err) throw err;
-//     console.log(res)
-// })
+app.use(bodyParser.json());
+
+// Local data files
+const songJSON = require("./asset/songs-data.json");
+const boyGroupJSON = require("./asset/boy-group.json");
+const girlGroupJSON = require("./asset/girl-group.json");
+const idolJSON = require("./asset/idol.json");
+
+// Scrape from website: Songs
+const fetchSongs = () => {
+    const child = spawn("./scrape/env-kpop/bin/python3.8", [
+        "./scrape/kpop_scrape_project/kpop_scrape_project/selenium_songs.py",
+    ]).on("error", (err) => {
+        console.log(
+            `Error Creating a child process from Node: ${err.toString()}`
+        );
+    });
+
+    child.stderr.on("data", (data) => {
+        console.log(`Error from Child process: ${data.toString()}`);
+    });
+};
+
+// Scrape from website: Boy Groups
+const fetchBoyGroups = () => {
+    const child = spawn("../../env-kpop/bin/scrapy", ["crawl", "boy-group"], {
+        cwd: "./scrape/kpop_scrape_project/kpop_scrape_project",
+    }).on("error", (d) => {
+        console.log(
+            `Error Creating a child process from Node: ${err.toString()}`
+        );
+    });
+
+    child.stdout.on("data", (d) => {
+        console.log(`Error from Child process: ${data.toString()}`);
+    });
+};
+
+// Scrape from website: Girl Groups
+const fetchGirlGroups = () => {
+    const child = spawn("../../env-kpop/bin/scrapy", ["crawl", "girl-group"], {
+        cwd: "./scrape/kpop_scrape_project/kpop_scrape_project",
+    }).on("error", (d) => {
+        console.log(
+            `Error Creating a child process from Node: ${err.toString()}`
+        );
+    });
+
+    child.stdout.on("data", (d) => {
+        console.log(`Error from Child process: ${data.toString()}`);
+    });
+};
+
+// Scrape from website: Idols
+const fetchIdols = () => {
+    const child = spawn("../../env-kpop/bin/scrapy", ["crawl", "idol"], {
+        cwd: "./scrape/kpop_scrape_project/kpop_scrape_project",
+    }).on("error", (d) => {
+        console.log(
+            `Error Creating a child process from Node: ${err.toString()}`
+        );
+    });
+
+    child.stdout.on("data", (d) => {
+        console.log(`Error from Child process: ${data.toString()}`);
+    });
+};
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////////////// REST API ///////////////////////////////
 
 // GET: Songs
 app.get("/songs", (req, res) => {
-    let spawn = require("child_process").spawn;
-    let child = spawn("./scrape/env-kpop/bin/python3.8", [
-        "./scrape/kpop_scrape_project/kpop_scrape_project/selenium_songs.py",
-    ]);
-    res.send("Hello!");
+    console.log(req.query.name)
+    res.send(songJSON)
 });
 
 // GET: Boy groups
 app.get("/boy-groups", (req, res) => {
-    let spawn = require("child_process").spawn;
+    
+    res.send(boyGroupJSON);
 
-    let child = spawn(
-        "../../env-kpop/bin/scrapy",
-        ["crawl", "boy-group", ],
-        {
-            cwd: "./scrape/kpop_scrape_project/kpop_scrape_project",
-        }
-    ).on('error', (d) => {
-        console.log(d.toString());
-    })
-
-    child.stdout.on("data", (d) => {
-        console.log(d.toString())
-    })
-
-    res.send("DAATTAAA");
 });
 
 // GET: Girl groups
+app.get("/girl-groups", (req, res) => {
+    res.send(girlGroupJSON);
+});
 
-// GET: Random
+// GET: Individual Idols
+app.get("/idols", (req, res) => {
+    res.send(idolJSON);
+});
 
-// GET: `
-
+// Express app Listening to Port
 app.listen(port, () => {
     console.log(`App running in ${port}`);
 });
--- scrape/kpop_scrape_project/kpop_scrape_project/selenium_songs.py
+++ scrape/kpop_scrape_project/kpop_scrape_project/selenium_songs.py
@@ -4,16 +4,27 @@ import time
 import json
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 
 site_url = 'https://dbkpop.com/db/k-pop-music-videos/'
 s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+
+chrome_options = Options()
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+chrome_options.add_argument(f'user-agent={user_agent}')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('window-size=1200,1100')
+driver = webdriver.Chrome(service=s, options=chrome_options)
 
 song_data = []
 
 driver.get(site_url)
+
+# Click the pop up
+
 
 # Change Drop-down menu to 'All' option 
 select = Select(driver.find_element(By.CSS_SELECTOR, 'select.wdt-selectpicker'))
@@ -21,34 +32,35 @@ select.select_by_visible_text('All')
 time.sleep(3)
 
 # For Each row (Skip first 2 rows)
-# for row in driver.find_elements(By.CSS_SELECTOR, '#table_1 tr')[2:4]:
-#     data = []
+for row in driver.find_elements(By.CSS_SELECTOR, '#table_1 tr')[2:]:
 
-#     for td in row.find_elements(By.CSS_SELECTOR, 'td'):
+    data = []
 
-#         # If data type is link, extract href instead of normal text
-#         if len(td.find_elements(By.CSS_SELECTOR, 'a')) > 0:
-#             data.append(td.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
-#         else:
-#             data.append(td.text)
-            
-#     col = [
-#         'Post',
-#         'Date',
-#         'Artist',
-#         'Song Name',
-#         'Korean Name',
-#         'Director', 
-#         'Video',
-#         'Type',
-#         'Release',
-#     ]
+    for td in row.find_elements(By.CSS_SELECTOR, 'td'):
 
-#     parsed_song_info = {k: data[i] for i, k in enumerate(col)}
-#     print(parsed_song_info)
+        # If data type is link, extract href instead of normal text
+        if len(td.find_elements(By.CSS_SELECTOR, 'a')) > 0:
+            data.append(td.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
+        else:
+            data.append(td.text)
 
-#     song_data.append(parsed_song_info)
+    col = [
+        'Post',
+        'Date',
+        'Artist',
+        'Song Name',
+        'Korean Name',
+        'Director', 
+        'Video',
+        'Type',
+        'Release',
+    ]
+
+    parsed_song_info = {k: data[i] for i, k in enumerate(col)}
+    print(parsed_song_info)
+
+    song_data.append(parsed_song_info)
 
 # Write JSON file
-with open('../songs-data.json', 'w', encoding='utf8') as f:
+with open('../../asset/songs-data.json', 'w', encoding='utf8') as f:
     json.dump(song_data, f, indent=4, ensure_ascii=True)
